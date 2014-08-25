099ea05475b06bcc7d975161fe96709e7c5ec8e7
Alexander Shabanov <avshabanov@gmail.com>
2014-08-24 23:44:18 -0700

    Simplifying config

--- service-core/src/test/java/com/truward/service/core/CalculatorServiceIntegrationTest.java
+++ service-core/src/test/java/com/truward/service/core/CalculatorServiceIntegrationTest.java
@@ -6,6 +6,7 @@ import com.truward.metrics.reader.MetricsReader;
 import com.truward.service.core.metrics.annotation.LogMetrics;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.stereotype.Service;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
